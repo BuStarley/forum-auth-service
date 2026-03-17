2b9cc2e96173f18ffbd71c08015c633a7f3017bd
Dmitry Repnin <dmitriyrepnin@mail.ru>
2026-03-17 13:58:02 +0300

    test: добавить @ActiveProfiles('test') в ForumAuthServiceApplicationTests

--- src/test/kotlin/com/example/forum_auth_service/ForumAuthServiceApplicationTests.kt
+++ src/test/kotlin/com/example/forum_auth_service/ForumAuthServiceApplicationTests.kt
@@ -2,8 +2,10 @@ package com.example.forum_auth_service
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ForumAuthServiceApplicationTests {
 
 	@Test
