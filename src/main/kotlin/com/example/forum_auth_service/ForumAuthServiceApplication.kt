b227c30ee9beb2eae64f21d557550d9f74a70284
Dmitry Repnin <dmitriyrepnin@mail.ru>
2026-03-17 13:55:56 +0300

    feat: добавить главный класс приложения
    - Создан ForumAuthServiceApplication с @SpringBootApplication
    - Добавлена main функция для запуска приложения
    - Класс необходим для корректной работы тестов и Spring контекста

---
+++ src/main/kotlin/com/example/forum_auth_service/ForumAuthServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.forum_auth_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForumAuthServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ForumAuthServiceApplication>(*args)
+}
